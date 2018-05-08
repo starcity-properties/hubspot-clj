6f4f7bae2c9e532581557edaa32a88925824dfaf
Josh Lehman <jalehman37@gmail.com>
2018-05-07 18:01:00 -0700

    throw errors on default

--- src/hubspot/http.clj
+++ src/hubspot/http.clj
@@ -147,33 +147,54 @@
     (json/parse-string (:body res) keyword)))
 
 
+(defn- process [res]
+  (try
+    (let [body (parse-response-body res)]
+      (if (= (:status body) "error")
+        {:error body}
+        body))
+    (catch Throwable t
+      {:error {:status     "error"
+               :message    (.getMessage t)
+               :error-data (ex-data t)}})))
+
+
+(defn- respond-sync
+  [params {:keys [throw-on-error?] :or {throw-on-error? true}}]
+  (let [response                @(http/request params)
+        {:keys [error] :as res} (process response)]
+    (if (and throw-on-error? (some? error))
+      (throw (ex-info (get-in res [:error :message]) res))
+      res)))
+
+
+(defn- respond-async
+  [params {:keys [out-ch throw-on-error?] :or {throw-on-error? true}}]
+  (http/request params
+                (fn [res]
+                  (let [{:keys [error] :as res} (process res)]
+                    (->> (if (and throw-on-error? (some? error))
+                           (ex-info (get-in res [:error :message]) res)
+                           res)
+                         (a/put! out-ch)))
+                  (a/close! out-ch))))
+
+
 (defn api-call
   "Call an API method on Hubspot. If an output channel is supplied, the method
   will place the result in that channel; if not, returns synchronously."
-  [{:keys [params client-options api-key method endpoint out-ch]
+  [{:keys [params client-options api-key method endpoint out-ch throw-on-error?]
     :or   {params         {}
            client-options {}
-           api-key        (api-key)}}]
+           api-key        (api-key)}
+    :as   opts}]
   (assert (some? api-key) "API Key must not be nil.")
-  (let [url     (url-endpoint endpoint)
-        params  (prepare-params api-key method params)
-        method  (get-http-method method)
-        process (fn [res]
-                  (try
-                    (let [body (parse-response-body res)]
-                      (if (= (:status body) "error")
-                        {:error body}
-                        body))
-                    (catch Throwable t
-                      {:error {:status     "error"
-                               :message    (.getMessage t)
-                               :error-data (ex-data t)}})))]
+  (let [url    (url-endpoint endpoint)
+        params (->> (prepare-params api-key method params)
+                    (merge {:method method :url url}))]
     (if-not (some? out-ch)
-      (process @(method url params))
-      (do (method url params
-                  (fn [ret]
-                    (a/put! out-ch (process ret))
-                    (a/close! out-ch)))
+      (respond-sync params opts)
+      (do (respond-async params opts)
           out-ch))))
 
 (s/fdef api-call
