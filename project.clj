dc1794b940c0d565d3d137173791ae7d9d3e9f95
Josh Lehman <jalehman37@gmail.com>
2018-09-19 15:34:20 -0700

    Version 0.3.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject starcity/hubspot-clj "0.3.2"
+(defproject starcity/hubspot-clj "0.3.3-SNAPSHOT"
   :description "Clojure bindings to the Hubspot API."
   :url "https://github.com/starcity-properties/hubspot-clj"
   :license {:name "Eclipse Public License"
