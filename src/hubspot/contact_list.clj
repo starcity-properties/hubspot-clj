01e92f9fc244e146f122794ef858f8f7b32d270b
Allen J Price <allenjprice@gmail.com>
2018-11-11 12:41:31 -0800

    implement ability to add contacts to a contact list

---
+++ src/hubspot/contact_list.clj
@@ -0,0 +1,60 @@
+(ns hubspot.contact-list
+  (:require [hubspot.http :as h]
+            [hubspot.spec :as hs]
+            [clojure.spec.alpha :as s]))
+
+
+;; ==============================================================================
+;; spec =========================================================================
+;; ==============================================================================
+
+
+(s/def ::list-id
+  integer?)
+
+(s/def ::vid
+  integer?)
+
+(s/def ::vids
+  (s/+ ::vid))
+
+(s/def ::email
+  hs/email?)
+
+(s/def ::emails
+  (s/+ ::email))
+
+(s/def ::add-contact-params
+  (s/keys :opt-un [::vids ::emails]))
+
+(s/def ::contact
+  (s/keys :req-un [::properties ::vid ::profile-url]
+          :opt-un [::identity-profiles ::form-submissions]))
+
+
+;; ==============================================================================
+;; HTTP API =====================================================================
+;; ==============================================================================
+
+
+(defn add-contact!
+  "Adds a contact (as identified by either a `:vid` or `:email` in `parmas`) to
+  the list with the provided `list-id`"
+  [list-id params opts]
+  (h/post-req
+   (format "contacts/v1/lists/%s/add" list-id)
+   (assoc opts :params params)))
+
+(s/fdef add-contact!
+        :args (s/cat :list-id ::list-id
+                     :params ::add-contact-params
+                     :opts h/request-options?)
+        :ret (hs/async ::contact))
+
+
+(defn fetch
+  "Fetch contact lists."
+  ([params]
+   (fetch params {}))
+  ([params opts]
+   (h/get-req "contacts/v1/lists" (assoc opts :params params))))
